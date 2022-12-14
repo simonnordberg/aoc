ab23924b0d85d7badcbd4f4be76a50e3b9f10f83
Simon Nordberg <hey@simonnordberg.com>
2022-12-14 09:40:33 +0100

    Refactor to use Point helper

--- aoc-python/2022/day14/solution.py
+++ aoc-python/2022/day14/solution.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+from itertools import pairwise
 from pathlib import Path
 
-from aoc.util import solution
+from aoc.util import solution, Point, sign, SOUTH, SOUTH_WEST, SOUTH_EAST
 
-SAND_ORIGIN = (500, 0)
+SAND_ORIGIN = Point(500, 0)
 ROCK = "#"
 SAND = "o"
 ORIGIN = "+"
@@ -12,73 +13,63 @@ ORIGIN = "+"
 
 def solve(cave, abyss_depth, cave_depth=None, target=None):
     while True:
-        sx, sy = SAND_ORIGIN
+        grain = SAND_ORIGIN
         while True:
             if cave_depth is not None:
-                cave[(sx, cave_depth)] = ROCK
-                cave[(sx - 1, cave_depth)] = ROCK
-                cave[(sx + 1, cave_depth)] = ROCK
+                cave[Point(grain.x, cave_depth)] = ROCK
+                cave[Point(grain.x - 1, cave_depth)] = ROCK
+                cave[Point(grain.x + 1, cave_depth)] = ROCK
 
-            if sy > abyss_depth:
-                print(f"Hit the abyss at depth {abyss_depth}")
+            if grain.y > abyss_depth:
                 return sum(1 for v in cave.values() if v == SAND)
 
-            if (sx, sy + 1) not in cave:  # down
-                sy += 1
+            if grain + SOUTH not in cave:
+                grain += SOUTH
                 continue
 
-            if (sx - 1, sy + 1) not in cave:  # down-left
-                sx -= 1
-                sy += 1
+            if grain + SOUTH_WEST not in cave:
+                grain += SOUTH_WEST
                 continue
 
-            if (sx + 1, sy + 1) not in cave:  # down-right
-                sx += 1
-                sy += 1
+            if grain + SOUTH_EAST not in cave:
+                grain += SOUTH_EAST
                 continue
 
-            cave[(sx, sy)] = SAND
+            cave[grain] = SAND
 
-            if target is not None and (sx, sy) == target:
+            if target is not None and grain == target:
                 return sum(1 for v in cave.values() if v == SAND)
 
             break
 
 
+def max_depth(cave) -> int:
+    return max({k: v for k, v in cave.items() if v == ROCK}, key=lambda k: k.y).y
+
+
 @solution(no=1)
 def solve_one(cave):
-    abyss_depth = max({k: v for k, v in cave.items() if v == ROCK}, key=lambda x: x[1])[1] + 1
-    return solve(cave, abyss_depth=abyss_depth)
+    return solve(cave, abyss_depth=max_depth(cave) + 1)
 
 
 @solution(no=2)
 def solve_two(cave):
-    cave_floor = max({k: v for k, v in cave.items() if v == ROCK}, key=lambda x: x[1])[1] + 2
-    return solve(cave, abyss_depth=cave_floor+1, cave_depth=cave_floor, target=(500, 0))
+    depth = max_depth(cave)
+    return solve(cave, abyss_depth=depth + 3, cave_depth=depth + 2, target=Point(500, 0))
 
 
 def parse_input(file='input'):
     cave = {SAND_ORIGIN: ORIGIN}
 
     for line in Path(file).read_text().strip().split("\n"):
-        waypoints = []
-        for point in line.strip().split("->"):
-            px, py = point.strip().split(",")
-            waypoints.append((int(px), int(py)))
-
-        for w in range(len(waypoints) - 1):
-            x1, y1 = waypoints[w]
-            x2, y2 = waypoints[w + 1]
-            dx, dy = x2 - x1, y2 - y1
-            if dx != 0:
-                step = int(dx / abs(dx))
-                for x in range(x1, x1 + dx + step, step):
-                    cave[(int(x), int(y1))] = ROCK
-            if dy != 0:
-                step = int(dy / abs(dy))
-                for y in range(y1, y1 + dy + step, step):
-                    cave[(int(x1), int(y))] = ROCK
-
+        waypoints = [Point(int(px), int(py))
+                     for px, py in (point.split(',')
+                                    for point in line.split('->'))]
+        for src, dst in pairwise(waypoints):
+            cave[src] = ROCK
+            while src != dst:
+                src += Point(sign(dst.x - src.x), sign(dst.y - src.y))
+                cave[src] = ROCK
     return cave
 
 
--- aoc-python/aoc/util.py
+++ aoc-python/aoc/util.py
@@ -1,4 +1,48 @@
 import functools
+from dataclasses import dataclass
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __abs__(self):
+        return Point(abs(self.x), abs(self.y))
+
+    def __eq__(self, other):
+        return other and self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+
+NORTH = Point(0, -1)
+NORTH_EAST = Point(1, -1)
+EAST = Point(1, 0)
+SOUTH_EAST = Point(1, 1)
+SOUTH = Point(0, 1)
+SOUTH_WEST = Point(-1, 1)
+WEST = Point(-1, 0)
+NORTH_WEST = Point(-1, -1)
+
+
+def sign(n):
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0
 
 
 def solution(no=1):
