dc78f9cf6b4bc3646bf630d4363e6f54dd42ee15
Simon Nordberg <simon@simonnordberg.com>
2023-10-24 08:43:15 +0200

    Update with template

--- aoc-python/2021/day5/solution.py
+++ aoc-python/2021/day5/solution.py
@@ -2,23 +2,22 @@ from __future__ import annotations
 
 from collections import Counter
 from copy import deepcopy
-from pathlib import Path
 
-from aoc.util import solution, sign, ints
+from aoc.util import solution, sign, ints, read_file
 
 
 def solve(data, diagonals=False):
     points = []
-    for row in data:
-        x1, y1, x2, y2 = ints(row)
+    for line in data:
+        x1, y1, x2, y2 = line
 
         if x1 == x2 or y1 == y2:
             for x in range(min(x1, x2), max(x1, x2) + 1):
                 for y in range(min(y1, y2), max(y1, y2) + 1):
                     points.append((x, y))
         elif diagonals:
-            dx = sign(x2-x1)
-            dy = sign(y2-y1)
+            dx = sign(x2 - x1)
+            dy = sign(y2 - y1)
 
             for x in range(x1, x2 + dx, dx):
                 points.append((x, y1))
@@ -37,11 +36,11 @@ def solve_two(data):
     return solve(data, diagonals=True)
 
 
-def parse_input(file='input'):
-    return Path(file).read_text().strip().split("\n")
+def parse(line):
+    return ints(line.strip())
 
 
 if __name__ == '__main__':
-    data = parse_input("input")
+    data = list(map(parse, read_file("input")))
     solve_one(deepcopy(data))
     solve_two(deepcopy(data))
