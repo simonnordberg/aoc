0398a624d130b58160dcedbbd82ed9508dc63f2a
Simon Nordberg <simon@simonnordberg.com>
2023-12-01 06:56:09 +0100

    2023 Day 1

---
+++ aoc-python/2023/day1/solve.py
@@ -0,0 +1,40 @@
+import sys
+
+from util import *
+
+
+def solve(xs):
+    return sum([int(f"{x[0]}{x[-1]}") for x in xs])
+
+
+@solution(no=1)
+def solve_one(xs):
+    return solve(xs)
+
+
+@solution(no=2)
+def solve_two(xs):
+    return solve(xs)
+
+
+def parse(line, replace=False):
+    if replace:
+        line = (
+            line.replace("one", "o1e")
+            .replace("two", "t2o")
+            .replace("three", "t3e")
+            .replace("four", "f4r")
+            .replace("five", "f5e")
+            .replace("six", "s6x")
+            .replace("seven", "s7n")
+            .replace("eight", "e8t")
+            .replace("nine", "n9e")
+        )
+
+    return ints("|".join([*line.strip()]))
+
+
+if __name__ == '__main__':
+    lines = sys.stdin.readlines()
+    solve_one(list(map(lambda x: parse(x, False), lines)))
+    solve_two(list(map(lambda x: parse(x, True), lines)))
