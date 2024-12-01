8a9f8c751a07ad4eadbc1f8dab04b9f30cddfee1
Simon Nordberg <simon@simonnordberg.com>
2024-12-01 09:47:44 +0100

    Use Counter

--- aoc-python/2024/day1/solve.py
+++ aoc-python/2024/day1/solve.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 lines = sys.stdin.read().strip().split("\n")
 
@@ -8,5 +9,6 @@ c1, c2 = sorted(c1), sorted(c2)
 p1 = sum(abs(a - b) for a, b in zip(c1, c2))
 print(p1)
 
-p2 = sum(a * c2.count(a) for a in c1)
+c2c = Counter(c2)
+p2 = sum(a * c2c[a] for a in c1)
 print(p2)
