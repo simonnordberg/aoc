6f75061300214b13b7e6aa405ddd4535ae91b2bd
Simon Nordberg <simon@simonnordberg.com>
2024-12-01 10:01:04 +0100

    2024 day 1

--- aoc-python/2024/day1/solve.py
+++ aoc-python/2024/day1/solve.py
@@ -1,5 +1,4 @@
 import sys
-from collections import Counter
 
 lines = sys.stdin.read().strip().split("\n")
 
@@ -9,6 +8,5 @@ c1, c2 = sorted(c1), sorted(c2)
 p1 = sum(abs(a - b) for a, b in zip(c1, c2))
 print(p1)
 
-c2c = Counter(c2)
-p2 = sum(a * c2c[a] for a in c1)
+p2 = sum(a * c2.count(a) for a in c1)
 print(p2)
